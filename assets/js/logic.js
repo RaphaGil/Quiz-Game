644da42a41b683965eb144e1c50096e134c36e9b
raphaelagil <raphaela.gil@yahoo.com>
2024-01-02 13:43:27 +0000

    organized the files and separeted the highscores.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -146,32 +146,7 @@ function endQuiz() {
       window.location.replace('./highscores.html')
     }})}
 
-    function check() {
-      const highscores = document.getElementById("highscores");
-      highscores.innerHTML = "";
-      
-      const storedInputs = JSON.parse(localStorage.getItem("storedInputs")) || [];
 
-      for (let i = 0; i < storedInputs.length; i++) {
-        const { initials, score } = storedInputs[i];
-        const listItem = document.createElement("li");
-        listItem.textContent = `${i + 1}. ${initials} : ${score}`;
-        highscores.appendChild(listItem);
-       
-      }
-      const clear = document.getElementById("clear");
-      clear.addEventListener("click", function () {
-        localStorage.removeItem("storedInputs");
-        localStorage.removeItem("finalScoreValue");
-        highscores.innerHTML = ""; 
-      });
-    }
-  
-    function finalCheck() {
-      console.log("hello")
-    check()
-    }
-    finalCheck();
 
 
   
